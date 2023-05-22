c3dab5ab8d549d896196a87600d9551f443960e3
nedagosa-cod <nestorcode4004@gmail.com>
2023-05-22 16:19:16 -0500

    test 3

--- src/locoScroll/locoScroll.js
+++ src/locoScroll/locoScroll.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
-import 'locomotive-scroll/src/locomotive-scroll.scss'
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
